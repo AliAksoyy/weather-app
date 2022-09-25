0c6bb3a55c3c2ca61e940275d4af70a8378c30d4
Ali <alifeyza4017@gmail.com>
2022-09-25 13:28:50 +0300

    added

--- app.js
+++ app.js
@@ -13,9 +13,27 @@ const setQuery =(e)=> {
 const getResult = (cityname) => {
   let query = `${url}weather?q=${cityname}&appid=${api_key}$unit=metric$lang=tr`
   console.log(query)
+
+  fetch(query).then((res)=> {
+    return res.json()
+  })
+  .then((data)=> dispalyResult(data))
 }
 
+const dispalyResult = (result)=> {
+  let city =document.querySelector(".city")
+  city.innerText = `${result.name}, ${result.sys.country}`
 
+  let temp = document.querySelector(".temp")
+  temp.innerText = `${Math.round(result.main.temp)} °C`;
+
+  let desc = document.querySelector(".desc")
+  desc.innerText = result.weather[0].description
+
+  let minmax = document.querySelector(".minmax")
+  minmax.innerText = `${Math.round(result.main.temp_min)} °C /
+  ${Math.round(result.main.temp_max)} ° C `
+}
 
 
 
