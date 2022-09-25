8d16494ff5336cc3ecf362a801871008e7c8f0c2
Ali <alifeyza4017@gmail.com>
2022-09-25 13:19:37 +0300

    added

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 
+const api_key = "5bb1e360c88ace5e3a7f0c8cfe9a34d7";
+const url = "https://api.openweathermap.org/data/2.5/"
 
-const url = "https://api.openweathermap.org/data/2.5/";
 
-const api_key = "5bb1e360c88ace5e3a7f0c8cfe9a34d7"
 
 const setQuery =(e)=> {
     if(e.keyCode ="13") {
       getResult(searchBar.value)
     }
+}
+
+const getResult = (cityname) => {
+  let query = `${url}weather?q=${cityname}&appid=${api_key}$unit=metric$lang=tr`
+  console.log(query)
 }
 
 
